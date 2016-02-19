edccdce8aceeb27e68b1e38281e003c88a71d021
Eleanor Demis <eleanordemis@gmail.com>
2016-02-19 14:43:49 -0800

    [bug] fixes ng repeat error, events are showing up in event view

--- client/allEventsList/eventsListCtrl.js
+++ client/allEventsList/eventsListCtrl.js
@@ -1,11 +1,12 @@
 angular.module('studyMate')
 
 .controller('eventsListCtrl',function($scope, eventsListFact){
-$scope.data = [];
+
   $scope.displayEvent = function(){
     console.log('++line 6 inside eventsListCtrl');
       eventsListFact.getEvents()
     .then(function(data){
+
       // var formatted = data.map(function(val) {
       //   var temp = JSON.parse(JSON.stringify(val));
       //   temp.datetime = moment(temp.datetime, moment.ISO_8601).utcOffset(480).format('MMM Do YYYY, h:mm A');
@@ -17,6 +18,7 @@ $scope.data = [];
       });
       $scope.data = data;
       console.log('++line 10 in eventsListCtrl Success: ',$scope.data);
+
     }).catch(function(err) {
       console.log('++line 12 in eventsList Ctrl Error: ',err);
     });
