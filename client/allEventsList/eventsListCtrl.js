6e722442747b1bb2b4b1af36739a64e6062fec61
bllnd <bllnd@mac.com>
2016-02-22 16:35:42 -0800

    [feature]implements chronological sorting of eventList

--- client/allEventsList/eventsListCtrl.js
+++ client/allEventsList/eventsListCtrl.js
@@ -6,12 +6,16 @@ $scope.data = [];
     console.log('++line 6 inside eventsListCtrl');
       eventsListFact.getEvents()
     .then(function(data){
-      var formatted = data.map(function(val) {
-        var temp = JSON.parse(JSON.stringify(val));
-        temp.datetime = moment(temp.datetime, moment.ISO_8601).utcOffset(480).format('MMM Do YYYY, h:mm A');
-        return temp;
+      // var formatted = data.map(function(val) {
+      //   var temp = JSON.parse(JSON.stringify(val));
+      //   temp.datetime = moment(temp.datetime, moment.ISO_8601).utcOffset(480).format('MMM Do YYYY, h:mm A');
+      //   return temp;
+      // });
+
+      data.forEach(function(value){
+        value.formatted = moment(value.datetime, moment.ISO_8601).utcOffset(480).format('MMM Do YYYY, h:mm A');
       });
-      $scope.data = formatted;
+      $scope.data = data;
       console.log('++line 10 in eventsListCtrl Success: ',$scope.data);
     }).catch(function(err) {
       console.log('++line 12 in eventsList Ctrl Error: ',err);
