33226b63dc1ec9ecdbaf1f20a8387805a01159d5
Hyungki <hyungkip@gmail.com>
2016-02-23 14:37:37 -0800

    [cleanup] removes unncessary comments and debugger

--- client/app.js
+++ client/app.js
@@ -38,30 +38,17 @@ angular.module('studyMate', ['ui.router'])
 })
 
 .run(function ($rootScope, $state, $location, logFact) {
-  debugger;
   $rootScope.url = "http://localhost:8000";
-
   $rootScope.$on('$stateChangeStart', function (e, toState) {
     //if authenticate prop is true but token doesnt exist
     if (toState.authenticate && !logFact.isAuth()) {
       e.preventDefault();
       $state.go('signin');
-      // $location.path('/signin');
     }
-
     //if authenticate prop is false but token exists
     if(!toState.authenticate && logFact.isAuth()) {
       e.preventDefault();
       $state.go('eventsHome');
     }
-    //
-    // if(!toState.send){
-    //   e.preventDefault();
-    //   $state.go('/eventsHome');
-    // }
-
-
   });
-
-
 });
